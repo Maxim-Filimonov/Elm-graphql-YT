7b30269234dd1b7bddd9ba993920b672dc511229
Maxim Filimonov <tpaktopsp@gmail.com>
2019-02-01 08:01:55 +1000

    wip not ready yet. setting up

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,35 @@
-module Main exposing (..)
+module Main exposing (Msg, main, update, view)
 
-import Html exposing(text)
+import Browser
+import Html exposing (text)
 
-view = text "Hello"
 
-type lias 
-update: Msg -> Model -> Model
-update msg model = model
+view =
+    text "Hello"
 
-main = Html.program
+
+type alias Msg =
+    Int
+
+
+type alias Model =
+    {}
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+init =
+    ( {}, Cmd.none )
+
+
+view : Model -> Html prog
+
+
+main =
+    Browser.element
         { init = init
         , update = update
         , subscriptions = \_ -> Sub.none
