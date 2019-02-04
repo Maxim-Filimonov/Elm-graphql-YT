c8436e3cc62768cc8034b310f7d7951bbc83a6e8
Maxim Filimonov <tpaktopsp@gmail.com>
2019-02-05 08:18:30 +1000

    add first query which actually works. need to add display to it

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,21 @@
 module Main exposing (Msg, main, update, view)
 
 import Browser
-import Html exposing (text)
+import Graphql.Document as Document
+import Graphql.Operation exposing (RootQuery)
+import Graphql.SelectionSet exposing (SelectionSet, with)
+import Html exposing (div, text)
+import Swapi.Object
+import Swapi.Object.Human as HumanGql
+import Swapi.Query as Query
+import Swapi.Scalar exposing (Id(..))
 
 
 view : OurModel -> Html.Html Msg
 view model =
-    text "Hello"
+    div []
+        [ text (Document.serializeQuery query)
+        ]
 
 
 type alias Msg =
@@ -29,6 +38,36 @@ type alias Flags =
 init : Flags -> ( OurModel, Cmd Msg )
 init flags =
     ( {}, Cmd.none )
+
+
+
+-- Graphql JAZZ
+
+
+query : SelectionSet (Maybe Human) RootQuery
+query =
+    Query.human { id = Id "1001" } humanSelection
+
+
+
+-- query requires datatype from the library
+
+
+humanSelection : SelectionSet Human Swapi.Object.Human
+humanSelection =
+    Graphql.SelectionSet.map2 Human
+        HumanGql.name
+        HumanGql.homePlanet
+
+
+type alias Human =
+    { name : String
+    , homePlanet : Maybe String
+    }
+
+
+
+-- rest of elm
 
 
 main =
